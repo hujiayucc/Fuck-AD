5fb9c2e6caee02f82c4425e15d3b052a14102f43
hujiayucc <hujiayucc@qq.com>
2023-03-05 18:05:11 +0800

    优化App分享

--- app/src/main/java/com/hujiayucc/hook/hook/app/AppShare.kt
+++ app/src/main/java/com/hujiayucc/hook/hook/app/AppShare.kt
@@ -1,8 +1,10 @@
 package com.hujiayucc.hook.hook.app
 
+import android.app.Activity
 import android.app.Application
 import android.content.Intent
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
+import com.highcapable.yukihookapi.hook.factory.MembersType
 import com.highcapable.yukihookapi.hook.type.android.ApplicationClass
 
 object AppShare : YukiBaseHooker() {
@@ -15,6 +17,29 @@ object AppShare : YukiBaseHooker() {
                     val intent = Intent(context, "info.muge.appshare.view.main.MainActivity".toClass(appClassLoader))
                         .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                     context.startActivity(intent)
+                }
+            }
+        }
+
+        findClass("info.muge.appshare.view.launch.LaunchActivity").hook {
+            injectMember {
+                allMembers(type = MembersType.METHOD)
+
+                beforeHook {
+                    val activity = instance<Activity>()
+                    activity.finish()
+                }
+            }
+        }
+
+        findClass("info.muge.appshare.view.main.MainActivity").hook {
+            injectMember {
+                method { name = "onBackPressed" }
+                replaceUnit {
+                    val intent = Intent(Intent.ACTION_MAIN)
+                    intent.addCategory(Intent.CATEGORY_HOME)
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                    instance<Activity>().startActivity(intent)
                 }
             }
         }
