4a0334740f133296b442b9d5d04caf615bf70b53
hujiayucc <hujiayucc@qq.com>
2023-04-06 01:26:23 +0800

    1.2.4

--- app/src/main/java/com/hujiayucc/hook/application/XYApplication.kt
+++ app/src/main/java/com/hujiayucc/hook/application/XYApplication.kt
@@ -1,6 +1,5 @@
 package com.hujiayucc.hook.application
 
-import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import com.highcapable.yukihookapi.hook.xposed.application.ModuleApplication
@@ -17,19 +16,16 @@ class XYApplication : ModuleApplication() {
         val filter = IntentFilter("com.hujiayucc.hook.service.StartService")
         registerReceiver(BootReceiver(), filter)
         sendBroadcast(Intent("com.hujiayucc.hook.service.StartService"))
-    }
 
-    override fun attachBaseContext(base: Context?) {
-        super.attachBaseContext(base)
         runCatching {
-            initHotFix(base!!.classLoader)
+            initHotFix(applicationContext.classLoader)
         }
     }
 
     /** 自动加载热更新 */
     private fun initHotFix(classLoader: ClassLoader) {
         // 删除旧版热更新
-        if (File(DEX_FILE,"base.dex").exists()) deleteOld(DEX_FILE)
+        if (File(DEX_FILE,"2023.dex").exists()) deleteOld(DEX_FILE)
         // 确保热更新文件夹存在
         if (!DEX_FILE.exists()) DEX_FILE.mkdirs()
         HotFixUtils().doHotFix(classLoader)
