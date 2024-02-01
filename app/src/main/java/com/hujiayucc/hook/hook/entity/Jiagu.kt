4aacd3e3d8c478a58dae2c67bd2f2cc946db91d5
hujiayucc <hujiayucc@qq.com>
2024-02-01 18:33:25 +0800

    Add Jiagu rule: 网易易盾

--- app/src/main/java/com/hujiayucc/hook/hook/entity/Jiagu.kt
+++ app/src/main/java/com/hujiayucc/hook/hook/entity/Jiagu.kt
@@ -2,5 +2,6 @@ package com.hujiayucc.hook.hook.entity
 
 enum class Jiagu(val packageName: String, val type: String) {
     Jiagu360("com.stub.StubApp","360加固"),
-    Tencent("com.wrapper.proxyapplication.WrapperProxyApplication","腾讯御安全");
+    Tencent("com.wrapper.proxyapplication.WrapperProxyApplication","腾讯御安全"),
+    WANGYI("com.netease.nis.wrapper.MyApplication","网易易盾");
 }
