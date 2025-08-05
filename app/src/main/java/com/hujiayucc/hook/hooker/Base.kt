dff946f5621873ad04029f736b137f577294d9fb
hujiayucc <hujiayucc@qq.com>
2025-08-06 01:32:01 +0800

    refactor(hook): 增加对 Hook 注解的兼容性处理
    - 新增 RunJiaGu 注解的处理逻辑
    - 优化版本检查和错误处理流程
    - 调整变量访问权限，提高封装性

--- app/src/main/java/com/hujiayucc/hook/hooker/Base.kt
+++ app/src/main/java/com/hujiayucc/hook/hooker/Base.kt
@@ -3,29 +3,34 @@ package com.hujiayucc.hook.hooker
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
 import com.highcapable.yukihookapi.hook.log.YLog
 import com.hujiayucc.hook.annotation.Run
+import com.hujiayucc.hook.annotation.RunJiaGu
 import com.hujiayucc.hook.utils.AppInfoUtil.appVersionName
 
 abstract class Base : YukiBaseHooker() {
-    lateinit var versions: Array<String>
-    lateinit var appName: String
-    lateinit var action: String
+    protected lateinit var versions: Array<String>
+    protected lateinit var appName: String
+    protected lateinit var action: String
+    protected val versionName get() = systemContext.appVersionName(packageName)
+
     override fun onHook() {
-        runCatching {
+        try {
             versions = this::class.java.annotations.filterIsInstance<Run>().first().versions
             appName = this::class.java.annotations.filterIsInstance<Run>().first().appName
             action = this::class.java.annotations.filterIsInstance<Run>().first().action
-            if (versions.isNotEmpty() && !versions.contains(versionName)) {
-                error("Hook Failed: $appName")
-                error("Current Version: $versionName")
-                error("Support Version: ${versions.contentToString()}")
-            } else {
-                debug("$appName => $action")
-                onStart()
-            }
+        } catch (e: NoSuchElementException) {
+            versions = this::class.java.annotations.filterIsInstance<RunJiaGu>().first().versions
+            appName = this::class.java.annotations.filterIsInstance<RunJiaGu>().first().appName
+            action = this::class.java.annotations.filterIsInstance<RunJiaGu>().first().action
+        }
+        if (versions.isNotEmpty() && !versions.contains(versionName)) {
+            error("Hook Failed: $appName")
+            error("Current Version: $versionName")
+            error("Support Version: ${versions.contentToString()}")
+        } else {
+            debug("$appName => $action")
+            onStart()
         }
     }
-
-    protected val versionName get() = systemContext.appVersionName(packageName)
 
     abstract fun onStart()
     fun debug(msg: String) {
