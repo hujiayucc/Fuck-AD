013ca2067a93c5c4d9c0cbb9e7b243231b70502b
hujiayucc <hujiayucc@qq.com>
2025-08-05 14:24:03 +0800

    feat(hook): 添加豆瓣 App 开屏广告跳过功能
    - 新增 DouBan 对象，实现豆瓣 App 开屏广告的跳过逻辑
    - 在 arrays.xml 中添加豆瓣 App 的包名
    - 通过 Hook 相关类和方法，实现广告跳过功能

---
+++ app/src/main/java/com/hujiayucc/hook/hooker/DouBan.kt
@@ -0,0 +1,123 @@
+package com.hujiayucc.hook.hooker
+
+import android.view.View
+import com.highcapable.yukihookapi.hook.factory.allMethods
+import com.highcapable.yukihookapi.hook.factory.method
+import com.hujiayucc.hook.annotation.Run
+
+@Run(
+    "豆瓣",
+    "com.douban.frodo",
+    "开屏广告",
+    [
+        "7.105.1"
+    ]
+)
+object DouBan : Base() {
+    override fun onStart() {
+        "com.bytedance.sdk.openadsdk.TTAdSdk".toClassOrNull()
+            ?.allMethods { _, method ->
+                method.hook {
+                    before {
+                        result = when (method.returnType) {
+                            java.lang.Boolean.TYPE -> false
+                            else -> null
+                        }
+                    }
+                }
+            }
+
+        "com.bytedance.sdk.openadsdk.api.ln".toClassOrNull()
+            ?.allMethods { _, method ->
+                method.hook {
+                    before {
+                        result = when (method.returnType) {
+                            java.lang.Boolean.TYPE -> false
+                            else -> null
+                        }
+                    }
+                }
+            }
+
+        "com.bytedance.sdk.openadsdk.core.AdSdkInitializerHolder".toClassOrNull()
+            ?.allMethods { _, method ->
+                method.hook {
+                    before {
+                        result = when (method.returnType) {
+                            java.lang.Boolean.TYPE -> false
+                            else -> null
+                        }
+                    }
+                }
+            }
+
+        "com.bytedance.sdk.openadsdk.CSJConfig".toClassOrNull()
+            ?.allMethods { _, method ->
+                method.hook {
+                    before {
+                        result = when (method.returnType) {
+                            java.lang.Boolean.TYPE -> false
+                            else -> null
+                        }
+                    }
+                }
+            }
+
+        "com.bytedance.sdk.openadsdk.AdSlot\$Builder".toClassOrNull()
+            ?.allMethods { _, method ->
+                method.hook {
+                    before {
+                        result = when (method.returnType) {
+                            java.lang.Boolean.TYPE -> false
+                            else -> null
+                        }
+                    }
+                }
+            }
+
+        "com.bytedance.sdk.openadsdk.core.component.splash.countdown.TTCountdownViewForCircle".toClassOrNull()
+            ?.method { name = "getView" }
+            ?.hook {
+                after {
+                    val view = instance as View
+                    view.performClick()
+                }
+            }
+
+        "com.qq.e.comm.managers.plugin.PM\$a".toClassOrNull()
+            ?.method { name = "call" }
+            ?.hook { replaceTo(null) }
+
+        "com.qq.e.comm.managers.plugin.PM\$a".toClassOrNull()
+            ?.method { name = "a" }
+            ?.hook { replaceToFalse() }
+
+        "com.douban.ad.h0".toClassOrNull()
+            ?.method { name = "run" }
+            ?.hook { replaceUnit {} }
+
+        "com.douban.ad.g0".toClassOrNull()
+            ?.method { name = "run" }
+            ?.hook { replaceUnit {} }
+
+        "com.douban.ad.k0".toClassOrNull()
+            ?.method { name = "d" }
+            ?.hook { replaceUnit {} }
+
+        "com.douban.ad.AdView".toClassOrNull()
+            ?.method { name = "b" }
+            ?.hook { replaceUnit {} }
+
+        "com.douban.ad.t".toClassOrNull()
+            ?.method { name = "onGlobalLayout" }
+            ?.hook { replaceUnit {} }
+
+        "com.douban.frodo.splash.SplashAdNewRequestor\$c".toClassOrNull()
+            ?.method { name = "handleMessage" }
+            ?.hook { replaceUnit{} }
+
+        "com.bytedance.sdk.openadsdk.core.component.splash.e.r$1".toClassOrNull()
+            ?.method { name = "run" }
+            ?.hook { replaceUnit {} }
+    }
+}
