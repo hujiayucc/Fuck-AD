2c3fd32b13a7e4aef0f65af5dfe0b713be7e4415
hujiayucc <hujiiayucc@qq.com>
2026-01-15 06:34:39 +0800

    refactor(Soul): 优化开屏广告Hook实现
    - 使用KavaRef库替换原有方法查找方式

--- app/src/main/java/com/hujiayucc/hook/hooker/Soul.kt
+++ app/src/main/java/com/hujiayucc/hook/hooker/Soul.kt
@@ -1,18 +1,19 @@
 package com.hujiayucc.hook.hooker
 
 import android.app.Activity
+import com.highcapable.kavaref.KavaRef.Companion.resolve
 import com.highcapable.yukihookapi.hook.factory.method
 import com.hujiayucc.hook.annotation.Run
 
 @Run(
-    "Soul",
-    "cn.soulapp.android",
-    "开屏广告"
+    appName = "Soul",
+    packageName = "cn.soulapp.android",
+    action = "开屏广告"
 )
 object Soul : Base() {
     override fun onStart() {
         "cn.soulapp.android.ad.ui.HotAdActivity".toClassOrNull()
-            ?.method { name = "onCreate" }
+            ?.resolve()?.firstMethod { name = "onCreate" }
             ?.hook {
                 before {
                     instance<Activity>().finish()
