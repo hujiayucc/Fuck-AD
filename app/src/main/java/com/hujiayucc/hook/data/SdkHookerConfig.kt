3f62d1e02d05bdd5b66c6b75485ba756c086e392
hujiayucc <hujiayucc@qq.com>
2026-07-01 01:49:02 +0000

    Add SDK hooker configuration entries

--- app/src/main/java/com/hujiayucc/hook/data/SdkHookerConfig.kt
+++ app/src/main/java/com/hujiayucc/hook/data/SdkHookerConfig.kt
@@ -8,11 +8,47 @@ object SdkHookerConfig {
     const val PANGLE = "pangle"
     const val GDT = "gdt"
     const val KW = "kw"
+    const val BAIDU = "baidu"
+    const val SIGMOB = "sigmob"
+    const val MINTEGRAL = "mintegral"
+    const val TOPON = "topon"
+    const val TRADPLUS = "tradplus"
+    const val GOOGLE = "google"
+    const val APPLOVIN = "applovin"
+    const val UNITY = "unity"
+    const val VUNGLE = "vungle"
+    const val LEVELPLAY = "levelplay"
 
     val sdkNames = linkedMapOf(
         PANGLE to "穿山甲",
         GDT to "腾讯广告",
-        KW to "快手广告"
+        KW to "快手广告",
+        BAIDU to "百青藤",
+        SIGMOB to "Sigmob",
+        MINTEGRAL to "Mintegral",
+        TOPON to "TopOn",
+        TRADPLUS to "TradPlus",
+        GOOGLE to "Google Ads",
+        APPLOVIN to "AppLovin MAX",
+        UNITY to "Unity Ads",
+        VUNGLE to "Vungle/Liftoff",
+        LEVELPLAY to "ironSource/LevelPlay"
+    )
+
+    val sdkComponentPrefixes = linkedMapOf(
+        PANGLE to listOf("com.bytedance.sdk.openadsdk."),
+        GDT to listOf("com.qq.e.ads", "com.qq.e.comm."),
+        KW to listOf("com.kwad.sdk.", "com.kwai.", "com.ksad."),
+        BAIDU to listOf("com.baidu.mobads."),
+        SIGMOB to listOf("com.sigmob."),
+        MINTEGRAL to listOf("com.mbridge.msdk."),
+        TOPON to listOf("com.anythink."),
+        TRADPLUS to listOf("com.tradplus.", "com.tradplusad."),
+        GOOGLE to listOf("com.google.android.gms.ads.", "com.google.android.gms.internal.ads."),
+        APPLOVIN to listOf("com.applovin."),
+        UNITY to listOf("com.unity3d.ads.", "com.unity3d.services."),
+        VUNGLE to listOf("com.vungle."),
+        LEVELPLAY to listOf("com.ironsource.", "com.unity3d.mediation.")
     )
 
     fun isEnabled(prefs: SharedPreferences, packageName: String, sdkId: String): Boolean {
